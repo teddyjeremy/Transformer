db069846476516abe1ad0fe0494eda447749f6db
teddyjeremy <84629999+teddyjeremy@users.noreply.github.com>
2026-08-04 15:16:20 +0300

    Training Configuration

--- config.py
+++ config.py
@@ -1,33 +1,57 @@
 from pathlib import Path
 
+
 def get_config():
     return {
         "batch_size": 8,
         "num_epochs": 20,
         "lr": 10**-4,
-        "seq_len": 350,
+        "seq_len": 128,
         "d_model": 512,
-        "datasource": 'opus_books',
+        "datasource": "opus_books",
         "lang_src": "en",
-        "lang_tgt": "it",
+        "lang_tgt": "fr",
         "model_folder": "weights",
         "model_basename": "tmodel_",
-        "preload": "latest",
+        "preload": None,
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel"
     }
 
-def get_weights_file_path(config, epoch: str):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
-    model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
 
-# Find the latest weights file in the weights folder
+def get_weights_file_path(config, epoch):
+    model_folder = Path(
+        f"{config['datasource']}_{config['model_folder']}"
+    )
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(model_folder / model_filename)
+
+
 def latest_weights_file_path(config):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
-    model_filename = f"{config['model_basename']}*"
-    weights_files = list(Path(model_folder).glob(model_filename))
-    if len(weights_files) == 0:
+    model_folder = Path(
+        f"{config['datasource']}_{config['model_folder']}"
+    )
+
+    if not model_folder.exists():
         return None
-    weights_files.sort()
-    return str(weights_files[-1])
+
+    weights_files = list(
+        model_folder.glob(
+            f"{config['model_basename']}*.pt"
+        )
+    )
+
+    if not weights_files:
+        return None
+
+    return str(
+        max(
+            weights_files,
+            key=lambda path: int(
+                path.stem.replace(
+                    config["model_basename"],
+                    ""
+                )
+            )
+        )
+    )
