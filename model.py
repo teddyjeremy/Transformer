30376b607f4dc947e5ad9830d3757ca197015f41
Jeremy Teddy <teddyjeremy12@gmail.com>
2026-08-04 11:07:50 +0300

    Add and Norm

--- model.py
+++ model.py
@@ -100,3 +100,12 @@ class MultiHeadAttention(nn.Module):
         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
 
         return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float):
+        super().__init__()
+        self.norm = LayerNorm()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
