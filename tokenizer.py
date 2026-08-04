3e432c41b3457e7aeb2595e02a980e13c62c3940
teddyjeremy <84629999+teddyjeremy@users.noreply.github.com>
2026-08-04 15:36:16 +0300

    Tokenizer Module

---
+++ tokenizer.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import WordLevelTrainer
+
+
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item["translation"][lang]
+
+
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(
+        config["tokenizer_file"].format(lang)
+    )
+
+    if not tokenizer_path.exists():
+        tokenizer = Tokenizer(
+            WordLevel(unk_token="[UNK]")
+        )
+        tokenizer.pre_tokenizer = Whitespace()
+
+        trainer = WordLevelTrainer(
+            special_tokens=[
+                "[UNK]",
+                "[PAD]",
+                "[SOS]",
+                "[EOS]"
+            ],
+            min_frequency=2
+        )
+
+        tokenizer.train_from_iterator(
+            get_all_sentences(ds, lang),
+            trainer=trainer
+        )
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(
+            str(tokenizer_path)
+        )
+
+    return tokenizer
