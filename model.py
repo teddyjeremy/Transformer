b06e718a378e4801c9ab61da859c75be514d7016
Jeremy Teddy <teddyjeremy12@gmail.com>
2026-08-04 11:47:45 +0300

    Decoder and Projection Layer

--- model.py
+++ model.py
@@ -132,3 +132,36 @@ class Encoder(nn.Module):
         for block in self.encoder_blocks:
             x = block(x, src_mask)
         return self.norm(x)
+
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttention, cross_attention_block: MultiHeadAttention, feed_forward_block: FeedForward, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])  
+
+    def forward(self, x, encoder_output, tgt_mask=None, src_mask=None):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, decoder_blocks: nn.ModuleList):
+        super().__init__()
+        self.decoder_blocks = decoder_blocks
+        self.norm = LayerNorm()
+
+    def forward(self, x, encoder_output, tgt_mask=None, src_mask=None):
+        for block in self.decoder_blocks:
+            x = block(x, encoder_output, tgt_mask, src_mask)
+        return self.norm(x)
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.linear = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        return torch.log_softmax(self.linear(x), dim=-1)
