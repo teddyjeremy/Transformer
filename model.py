859e54d4450097d67d1ca8bae5d06ce4fae788e3
Jeremy Teddy <teddyjeremy12@gmail.com>
2026-08-04 10:57:39 +0300

    Multihead Attention

--- model.py
+++ model.py
@@ -54,3 +54,49 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+
+        assert d_model % h == 0, "d_model must be divisible by h"
+        self.d_k = d_model // h
+
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+
+    @staticmethod
+    def attention(query, key, value, mask=None, dropout=None):
+        d_k = query.size(-1)
+        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+
+        p_attn = torch.softmax(scores, dim=-1)
+
+        if dropout is not None:
+            p_attn = dropout(p_attn)
+
+        return torch.matmul(p_attn, value), p_attn
+
+    def forward(self, q, k, v, mask=None):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.p_attn = MultiHeadAttention.attention(query, key, value, mask=mask, dropout=self.dropout)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        return self.w_o(x)
