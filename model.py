635562379c154e4871df3772e66721d15fcf3f06
Jeremy Teddy <teddyjeremy12@gmail.com>
2026-08-04 09:58:17 +0300

    Feed Foward

--- model.py
+++ model.py
@@ -44,3 +44,13 @@ class LayerNorm(nn.Module):
         mean = x.mean(dim=-1, keepdim=True)
         std = x.std(dim=-1, keepdim=True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
