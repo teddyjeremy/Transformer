38f25000fead657c1fdb6874b5ce8d0dc363fa8e
teddyjeremy <84629999+teddyjeremy@users.noreply.github.com>
2026-08-04 15:36:30 +0300

    Tokenizer Pipeline Refactor

--- train.py
+++ train.py
@@ -5,10 +5,6 @@ import torch
 import torch.nn as nn
 import torchmetrics
 from datasets import load_dataset
-from tokenizers import Tokenizer
-from tokenizers.models import WordLevel
-from tokenizers.pre_tokenizers import Whitespace
-from tokenizers.trainers import WordLevelTrainer
 from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
@@ -20,6 +16,7 @@ from config import (
 )
 from dataset import BilingualDataset, causal_mask
 from model import build_transformer
+from tokenizer import get_or_build_tokenizer
 
 
 def greedy_decode(
@@ -148,45 +145,6 @@ def run_validation(
             global_step
         )
         writer.flush()
-
-
-def get_all_sentences(ds, lang):
-    for item in ds:
-        yield item["translation"][lang]
-
-
-def get_or_build_tokenizer(config, ds, lang):
-    tokenizer_path = Path(
-        config["tokenizer_file"].format(lang)
-    )
-
-    if not tokenizer_path.exists():
-        tokenizer = Tokenizer(
-            WordLevel(unk_token="[UNK]")
-        )
-        tokenizer.pre_tokenizer = Whitespace()
-
-        trainer = WordLevelTrainer(
-            special_tokens=[
-                "[UNK]",
-                "[PAD]",
-                "[SOS]",
-                "[EOS]"
-            ],
-            min_frequency=2
-        )
-
-        tokenizer.train_from_iterator(
-            get_all_sentences(ds, lang),
-            trainer=trainer
-        )
-        tokenizer.save(str(tokenizer_path))
-    else:
-        tokenizer = Tokenizer.from_file(
-            str(tokenizer_path)
-        )
-
-    return tokenizer
 
 
 def get_ds(config):
